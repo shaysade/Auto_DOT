e88c3af7032b0c084f4c5bb9b4bf6e2af11fb89e
shaysade <shaysade22@gmail.com>
2024-02-28 14:56:31 +0200

    adding dependencies

--- Demo_Run.py
+++ Demo_Run.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
-from playwright.sync_api import sync_playwright
 from concurrent.futures import ThreadPoolExecutor
 import subprocess
 import json
@@ -55,63 +54,6 @@ def quiality_dashboad():
         for feature, kpi in main_app_kpis.items():
             st.text(f"{feature}: {kpi}")
 
-
-
-# def run_playwright_sync():
-#     result = ""
-#     try:
-#         with sync_playwright() as p:
-#             browser = p.chromium.launch(headless=False)  # Adjust headless as needed
-#             page = browser.new_page()
-#             page.goto('https://www.ebay.com')
-#             page.fill('input[id="gh-ac"]', 'vintage watch')
-#             page.click('input[id="gh-btn"]')
-#             page.wait_for_navigation()
-#             # Verify the results...
-#             if 'vintage watch' in page.title().lower():
-#                 result = "Test Passed: The results page displays listings related to 'vintage watch'."
-#             else:
-#                 result = "Test Failed: The page title does not contain 'vintage watch'."
-#             browser.close()
-#     except Exception as e:
-#         result = f"An error occurred: {str(e)}"
-#     return result
-
-# def test_ebay_search_for_vintage_watch():
-#     # Run Playwright in a separate thread
-#     with ThreadPoolExecutor() as executor:
-#         future = executor.submit(run_playwright_sync)
-#         result = future.result()  # Wait for completion
-#         st.write(result)
-
-
-# def test_ebay_search_for_vintage_watch():
-#     with sync_playwright() as p:
-#         # Launch the browser
-#         browser = p.chromium.launch(headless=False)  # Set headless=False to see the browser
-#         page = browser.new_page()
-
-#         # Step 1: Navigate to eBay homepage
-#         page.goto('https://www.ebay.com')
-
-#         # Step 2: Enter 'vintage watch' in the search bar
-#         page.fill('input[id="gh-ac"]', 'vintage watch')
-
-#         # Step 3: Press the search button
-#         page.click('input[id="gh-btn"]')
-#         page.wait_for_navigation()
-
-#         # Expected Result: The results page displays listings related to 'vintage watch'.
-#         assert 'vintage watch' in page.title().lower(), "The page title does not contain 'vintage watch'"
-
-#         # Optionally, check if listings are indeed displayed
-#         # This example assumes there's a class for listing items, you'll need to replace '.listing-item' with the correct selector
-#         listings_count = page.locator('.s-item').count()
-#         assert listings_count > 0, "No listings found for 'vintage watch'."
-
-#         # Close the browser
-#         browser.close()                    
-                            
 
 def tests_suite():
     st.title("Tests Suite")
