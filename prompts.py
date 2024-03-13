b2cacab8a34a8a78a946549908b59af6813d3370
shaysade <shaysade22@gmail.com>
2024-03-13 13:43:27 +0200

    improve test cases prompt

--- prompts.py
+++ prompts.py
@@ -1,4 +1,4 @@
-test_cases_prompts = """ You are an test automation engineer co-pilot. You need to generate test cases from a PRD document and playwright code
+test_cases_prompts = """ You are qa expert co-pilot. You need to generate test cases from a PRD document and playwright code
                         Pay attention to additional instruction provided
 
                         The PRD text is: {prd_text}
@@ -8,7 +8,8 @@ test_cases_prompts = """ You are an test automation engineer co-pilot. You need 
                         Generate 10 test cases for the happy flow, 10 negative tests and 10 edge case tests.
                         For each test case provide a title, a description, a precondition, a list of steps and an expected result
                         return a json in the right format. The steps are just a list of strings
-                        
+                        create tests as action detailed as possible - use real data from PRD,playwright code and Additional Instructions 
+                        and integrate it to test cases data. 
                         Additional Instructions: {additional_instructions}
                         """
 
